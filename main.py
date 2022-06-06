0a6d7bf5669d4dd837649426539e97f4aba7c338
Anthony Susevski <asusevski@gmail.com>
2022-06-06 00:11:46 -0400

    added loop so program keeps going after one run through

--- main.py
+++ main.py
@@ -10,41 +10,48 @@ def main() -> None:
         print("No transcript found.")
         return
     
-
-    courses = []
-    print("Enter the course subject(s) -- for all subjects, press enter: ")
+    print("Enter q to quit at any time.")
     while True:
-        course = input("> ")
-        if not course:
-            break
-        course = course.lower()
-        print(f"You entered: {course}. To stop entering courses, press enter.")
-        courses.append(course)
-    print("Enter lower bound of course number (ie: to include 3rd year courses and above, enter 300).: ")
-    try:
-        lower = input("> ")
-        if not lower:
-            lower = 0
-        else:
-            lower = int(lower)
-    except ValueError:
-        print("Please enter a number. Exiting.")
-        return
-    
-    print("Enter upper bound of course number (ie: to include up until 4th year courses, enter 499): ")
-    try:
-        upper = input("> ")
-        if not upper:
-            # Arbitrary large number, not 499 in case grad classes :)
-            upper = 10000
-        else:
-            upper = int(upper)
-    except ValueError:
-        print("Please enter a number. Exiting.")
-        return
-    
-    grades = transcript.get_grades(courses, lower, upper)
-    print(f"{np.mean(list(grades.values())):.2f}")
+        courses = []
+        print("Enter the course subject(s) -- for all subjects, press enter: ")
+        while True:
+            course = input("> ")
+            if not course:
+                break
+            if course.lower() == "q":
+                return
+            course = course.lower()
+            print(f"You entered: {course}. To stop entering courses, press enter.")
+            courses.append(course)
+        print("Enter lower bound of course number (ie: to include 3rd year courses and above, enter 300).: ")
+        try:
+            lower = input("> ")
+            if not lower:
+                lower = 0
+            elif lower.lower() == "q":
+                return
+            else:
+                lower = int(lower)
+        except ValueError:
+            print("Please enter a number. Exiting.")
+            return
+        
+        print("Enter upper bound of course number (ie: to include up until 4th year courses, enter 499): ")
+        try:
+            upper = input("> ")
+            if not upper:
+                # Arbitrary large number, not 499 in case grad classes :)
+                upper = 10000
+            elif upper.lower() == "q":
+                return
+            else:
+                upper = int(upper)
+        except ValueError:
+            print("Please enter a number. Exiting.")
+            return
+        
+        grades = transcript.get_grades(courses, lower, upper)
+        print(f"{np.mean(list(grades.values())):.2f}")
 
 
 if __name__ == '__main__':
